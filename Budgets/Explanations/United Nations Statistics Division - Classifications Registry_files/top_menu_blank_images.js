68aa78baaea9ddf6c135597e3075b54d17d07cf0
NOLEFP <RRADOVANOVIC@COLLEGE.HARVARD.EDU>
2016-04-11 20:24:44 -0400

    Initial Upload of Everything

---
+++ Budgets/Explanations/United Nations Statistics Division - Classifications Registry_files/top_menu_blank_images.js
@@ -0,0 +1,248 @@
+﻿function cdd_menu109707(){//////////////////////////Start Menu Data/////////////////////////////////
+
+//**** NavStudio, (c) 2004, OpenCube Inc.,  -  www.opencube.com ****
+//Note: This data file may be manually customized outside of the visual interface.
+
+    //Unique Menu Id
+	this.uid = 109707
+
+
+/**********************************************************************************************
+
+                               Icon Images
+
+**********************************************************************************************/
+
+
+
+
+
+
+/**********************************************************************************************
+
+                              Global - Menu Container Settings
+
+**********************************************************************************************/
+
+
+	this.menu_background_color = "#cfcfcf"
+	this.menu_border_color = "#ffffff"
+	this.menu_border_width = "1"
+	this.menu_padding = "1,0,0,0"
+	this.menu_border_style = "none"
+	this.divider_caps = false
+	this.divider_width = 0
+	this.divider_height = 0
+	this.divider_background_color = "#ffffff"
+	this.divider_border_style = "groove"
+	this.divider_border_width = "1"
+	this.divider_border_color = "#000000"
+	this.menu_is_horizontal = false
+	this.menu_width = "130"
+	this.menu_xy = "-100,-2"
+	this.menu_scroll_direction = 1
+	this.menu_scroll_reverse_on_hide = true
+	this.menu_scroll_delay = 0
+	this.menu_scroll_step = 5
+
+
+
+
+/**********************************************************************************************
+
+                              Global - Menu Item Settings
+
+**********************************************************************************************/
+
+
+	this.menu_items_background_color_roll = "#efefef"
+	this.menu_items_text_color = "#0f0f0f"
+	this.menu_items_text_decoration = "none"
+	this.menu_items_font_family = "Tahoma"
+	this.menu_items_font_size = "11px"
+	this.menu_items_font_style = "normal"
+	this.menu_items_font_weight = "normal"
+	this.menu_items_text_align = "left"
+	this.menu_items_padding = "4,7,4,7"
+	this.menu_items_border_style = "none"
+	this.menu_items_border_color = "#9f9f9f"
+	this.menu_items_border_width = "0"
+	this.menu_items_width = 110
+	this.menu_items_background_color = "#b9dfea"
+	this.menu_items_text_color_roll = "#000000"
+
+
+
+
+/**********************************************************************************************
+
+                              Main Menu Settings
+
+**********************************************************************************************/
+
+
+        this.menu_background_color_main = "transparent"
+        this.menu_items_background_color_main = "transparent"
+        this.menu_items_background_color_roll_main = "transparent"
+        this.menu_border_style_main = "none"
+        this.menu_items_border_style_main = "none"
+        this.menu_border_width_main = 0
+        this.menu_items_border_width_main = 0
+        this.menu_padding_main = "0,0,0,0"
+        this.menu_items_padding_main = "0,0,0,0"
+        this.menu_is_horizontal_main = true
+        this.divider_width_main = 0
+        this.divider_height_main = 0
+        this.divider_caps_main = false
+        this.divider_border_style_main = "none"
+        this.divider_border_width_main = "0"
+
+        this.item0 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_grey_home_verdana.png' width=79 height=29>"
+        this.item_roll0 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_blue_home_verdana.png' width=79 height=29>"
+        this.item1 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_grey_databases_verdana.png' width=79 height=29>"
+        this.item_roll1 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_blue_databases_verdana.png' width=79 height=29>"
+        this.item2 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_grey_publications_verdana.png' width=79 height=29>"
+        this.item_roll2 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_blue_publications_verdana.png' width=79 height=29>"
+        this.item3 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_grey_inter_standards_verdana.png' width=89 height=29>"
+        this.item_roll3 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_blue_inter_standards_verdana.png' width=89 height=29>"
+        this.item4 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_grey_inter_meetings_verdana.png' width=79 height=29>"
+        this.item_roll4 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_blue_inter_meetings_verdana.png' width=79 height=29>"
+        this.item5 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_grey_tech_newsletters_verdana.png' width=79 height=29>"
+        this.item_roll5 = "<img vqp_img=1 src='"+cdd__codebase109707+"img/Blue_Bottom_blue_tech_newsletters_verdana.png' width=79 height=29>"
+
+        this.item_width0 = 79
+        this.item_width1 = 79
+        this.item_width2 = 79
+        this.item_width3 = 89
+        this.item_width4 = 79
+        this.item_width5 = 79
+
+        this.url0 = "default.htm"
+        this.url_target0 = "_self"
+        this.url1 = "databases.htm"
+        this.url_target1 = "_self"
+        this.url2 = "publications/"
+        this.url_target2 = "_self"
+        this.url3 = "methods.htm"
+        this.url_target3 = "_self"
+        this.url4 = "http://unstats.un.org/unsd/events/"
+        this.url_target4 = "_self"
+        this.url5 = "newsletters_main.htm"
+        this.url_target5 = "_self"
+
+
+
+
+/**********************************************************************************************
+
+                              Sub Menu Settings
+
+**********************************************************************************************/
+
+
+    //Sub Menu 0
+
+
+
+
+    //Sub Menu 1
+
+        this.menu_xy1 = "-77,7"
+        this.menu_width1 = 190
+        this.menu_is_horizontal1 = false
+        this.divider_caps1 = false
+        this.divider_border_style1 = "groove"
+
+        this.item1_0 = "Classifications Registry"
+        this.item1_1 = "Joint Oil Data Initiative (JODI) "
+        this.item1_2 = "Millennium Indicators"
+        this.item1_3 = "Monthly Bulletin of Statistics Online"
+        this.item1_4 = "National Accounts Main Aggregates"
+        this.item1_5 = "UN Comtrade"
+
+        this.item_background_color1_0 = "#b9dfea"
+
+        this.url1_0 = "cr/registry/default.asp?Lg=1"
+        this.url_target1_0 = "_self"
+        this.url1_1 = "jodi/"
+        this.url1_2 = "http://mdgs.un.org"
+        this.url_target1_2 = "_self"
+        this.url1_3 = "mbs/"
+        this.url_target1_3 = "_self"
+        this.url1_4 = "snaama/Introduction.asp"
+        this.url_target1_4 = "_self"
+        this.url1_5 = "http://comtrade.un.org/"
+
+
+
+    //Sub Menu 2
+
+
+
+
+    //Sub Menu 3
+
+
+
+
+    //Sub Menu 4
+
+
+
+
+    //Sub Menu 5
+
+        this.menu_xy5 = "-76,7"
+        this.menu_width5 = 170
+        this.divider_caps5 = true
+        this.divider_border_style5 = "double"
+        this.divider_border_width5 = "0"
+
+        this.item5_0 = "Classifications"
+        this.item5_1 = "Demographic & Social"
+        this.item5_2 = "Energy "
+        this.item5_3 = "Environment "
+        this.item5_4 = "International Trade "
+        this.item5_5 = "International Trade in Services"
+        this.item5_6 = "National Accounts"
+
+        this.url5_0 = "class/intercop/newsletter/default.asp"
+        this.url_target5_0 = "_self"
+        this.url5_1 = "demographic/products/newsletter/default.htm"
+        this.url_target5_1 = "_self"
+        this.url5_2 = "energy/Newsletter/default.htm"
+        this.url_target5_2 = "_self"
+        this.url5_3 = "ENVIRONMENT/newsletters.htm"
+        this.url_target5_3 = "_self"
+        this.url5_4 = "trade/newsletter.htm"
+        this.url_target5_4 = "_self"
+        this.url5_5 = "tradeserv/tfsits/newsletter.htm"
+        this.url5_6 = "nationalaccount/snanews.asp"
+        this.url_target5_6 = "_self"
+
+
+
+}///////////////////////// END Menu Data /////////////////////////////////////////
+
+
+
+//Document Level Settings
+
+cdd__activate_onclick = false
+cdd__showhide_delay = 100
+cdd__url_target = "_self"
+cdd__url_features = "resizable=1, scrollbars=1, titlebar=1, menubar=1, toolbar=1, location=1, status=1, directories=1, channelmode=0, fullscreen=0"
+cdd__display_urls_in_status_bar = true
+cdd__default_cursor = "hand"
+cdd__go = true
+
+
+//NavStudio Code (Warning: Do Not Alter!)
+
+if (window.showHelp){b_type = "ie"; if (!window.attachEvent) b_type += "mac";}if (document.createElementNS) b_type = "dom";if (navigator.userAgent.indexOf("afari")>-1) b_type = "safari";if (window.opera) b_type = "opera"; qmap1 = "\<\script language=\"JavaScript\" vqptag='loader_sub' src=\""; qmap2 = ".js\">\<\/script\>";;function iesf(){};;function vqp_error(val){alert(val)}
+if (b_type){document.write(qmap1+cdd__codebase+"pbrowser_"+b_type+qmap2);document.close();}
+
+
+	function iesf(show_obj,show){if (show_obj.canHaveHTML){fuid = show_obj.id+'_iesf';if (show){document.body.insertAdjacentHTML("afterBegin","<iframe scrolling=no id='"+fuid+"' style='position:absolute; top:0; left:0; visibility:hidden; z-index:1; filter:progid:DXImageTransform.Microsoft.Alpha(Opacity=0);'></iframe>");(wobj = document.getElementById(fuid).style).left = show_obj.offsetLeft;wobj.top = show_obj.offsetTop;wobj.width = show_obj.offsetWidth;wobj.height = show_obj.offsetHeight;wobj.visibility = 'visible';}else document.getElementById(fuid).removeNode(true);}}
+	if (window.showHelp){b_type = "ie"; if (!window.attachEvent) b_type += "mac";}if (document.createElementNS) b_type = "dom";if (navigator.userAgent.indexOf("afari")>-1) b_type = "safari";if (window.opera) b_type = "opera"; qmap1 = "\<\script language=\"JavaScript\" vqptag='loader_sub' src=\""; qmap2 = ".js\">\<\/script\>";;function vqp_error(val){alert(val)}
+	if (b_type){document.write(qmap1+cdd__codebase+"pbrowser_"+b_type+qmap2);document.close();}
